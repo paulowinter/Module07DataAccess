6f02906c59686506b73f63d01a625b18ed830d0d
Paulo Bondoc <paulo.pikatchu@gmail.com>
2024-10-16 13:07:39 +0800

    Add project files.

---
+++ Module07DataAccess/Services/DatabaseConnectionService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module07DataAccess.Services
+{
+    public class DatabaseConnectionService
+    {
+        private readonly string _connectionString;
+
+        public DatabaseConnectionService()
+        {
+            _connectionString = "Server=localhost;Database=testDB;User ID=testuser;Password=testuser";
+        }
+
+        public string GetConnectionString()
+        {
+            return _connectionString;
+        }
+
+    }
+}
